86d538f0ebaade36462351eff083c3689361581b
Bluefinger <bluefinger@gmail.com>
2021-09-09 14:17:12 +0200

    Refactor petri-simulation into a bevy plugin

--- crates/petri_simulation/src/lib.rs
+++ crates/petri_simulation/src/lib.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use std::{f32::consts::PI, iter::repeat_with};
+
+use bevy::{prelude::*, render::camera::WindowOrigin};
+use petri_rand::PetriRand;
 
 #[derive(Debug, Default)]
 pub struct Simulation {
@@ -31,6 +34,116 @@ pub struct CreatureBundle {
 #[derive(Debug, Default)]
 pub struct Velocity {
     pub vector: Vec3,
+}
+
+struct Materials {
+    creature: Handle<ColorMaterial>,
+    food: Handle<ColorMaterial>,
+}
+
+fn startup_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let _scenes: Vec<HandleUntyped> = asset_server.load_folder("sprites/").unwrap();
+
+    commands.insert_resource(Materials {
+        creature: materials.add(asset_server.get_handle("sprites/creature.png").into()),
+        food: materials.add(asset_server.get_handle("sprites/food.png").into()),
+    });
+
+    commands.insert_resource(Simulation {
+        world: Vec2::splat(800.0),
+        creatures: 40,
+        food: 60,
+    });
+
+    let mut camera_bundle = OrthographicCameraBundle::new_2d();
+
+    camera_bundle.orthographic_projection.window_origin = WindowOrigin::BottomLeft;
+
+    commands
+        // And use an orthographic projection
+        .spawn_bundle(camera_bundle);
+}
+
+fn creature_setup(mut commands: Commands, materials: Res<Materials>, sim: Res<Simulation>) {
+    let rng = PetriRand::thread_local();
+
+    repeat_with(|| {
+        let translation = Vec3::new(
+            rng.get_f32() * sim.world.x,
+            rng.get_f32() * sim.world.y,
+            1.0,
+        );
+        let rotation = Quat::from_rotation_z(rng.get_f32() * 2.0 * PI);
+        let scale = Vec2::splat(0.07).extend(1.0);
+
+        CreatureBundle {
+            sprite: SpriteBundle {
+                material: materials.creature.clone(),
+                transform: Transform {
+                    translation,
+                    rotation,
+                    scale,
+                },
+                ..Default::default()
+            },
+            ..Default::default()
+        }
+    })
+    .take(sim.creatures)
+    .for_each(|bundle| {
+        commands.spawn_bundle(bundle);
+    });
+}
+
+fn food_setup(mut commands: Commands, materials: Res<Materials>, sim: Res<Simulation>) {
+    let rng = PetriRand::thread_local();
+
+    repeat_with(|| {
+        let translation = Vec3::new(
+            rng.get_f32() * sim.world.x,
+            rng.get_f32() * sim.world.y,
+            0.0,
+        );
+        let scale = Vec2::splat(0.05).extend(1.0);
+
+        FoodBundle {
+            sprite: SpriteBundle {
+                material: materials.food.clone(),
+                transform: Transform {
+                    translation,
+                    scale,
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            ..Default::default()
+        }
+    })
+    .take(sim.food)
+    .for_each(|bundle| {
+        commands.spawn_bundle(bundle);
+    });
+}
+
+#[derive(Clone, Hash, Debug, PartialEq, Eq, StageLabel)]
+struct SimulationSetupStage;
+
+pub struct SimulationPlugin;
+
+impl Plugin for SimulationPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(startup_system.system())
+            .add_startup_stage(
+                SimulationSetupStage,
+                SystemStage::parallel()
+                    .with_system(creature_setup.system())
+                    .with_system(food_setup.system()),
+            );
+    }
 }
 
 // #[cfg(test)]
